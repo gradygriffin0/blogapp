1a049e1e41c800c7ab541b3321f250f3a2fdbe4d
gradygriffin0 <grady.griffin0@gmail.com>
2021-08-11 11:03:55 -0500

    adds controller practice

---
+++ src/main/java/com/codeup/blogapp/HelloController.java
@@ -0,0 +1,34 @@
+package com.codeup.blogapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+
+    @ResponseBody
+    public String hello(){
+        return "Hello from Spring!";
+    }
+
+
+    @GetMapping("/person")
+    @ResponseBody
+    public Person getPerson(){
+        return new Person("Casey", "Bum");
+    }
+
+    // {name} in mapping makes the endpoint dynamic when coupled with @PathVariable
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    // PathVariable in parameter
+    public String sayHello(@PathVariable String name){
+        return "Hello " + name + "!";
+    }
+
+}
+
+
